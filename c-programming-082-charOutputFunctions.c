70aada5a2dea6828f0c302fd09514fea045e41e2
Tomasz Jagas <tomasz.jagas@gmail.com>
2021-02-08 22:10:31 +0100

    first commit

---
+++ c-programming-082-charOutputFunctions.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    // putc
+    char ch = 'a';
+    
+    putc(ch, stdout);
+
+    // putchar
+    char string[]= "Hello, \n whatever!";
+    int i = 0;
+
+    while (string[i] != '\0') {
+        if (string[i] != '\n') {
+            putchar(string[i]);
+        }
+        ++i;
+    }
+
+    // putchar ex 2
+    int ch1 = 0;
+    while ((ch1 = getchar()) != EOF) {
+        putchar(ch1);
+    }
+
+    ungetc(ch, stdin);
+    printf("Thank you!\n");
+
+    //fputc
+    FILE *pFile = NULL;
+    char c = '\0';
+
+    pFile = fopen("myFile.txt", "w");
+
+    if (pFile != NULL) {
+        for (c = 'A'; c <= 'Z'; c++) {
+            fputc(c, stdout);
+        }
+        fclose(pFile);
+    }
+
+    return 0;
+}
